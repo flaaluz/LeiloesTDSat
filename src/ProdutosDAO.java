6fc4ae1ddfd26f500aa0c10f9d2326afa59d467d
Flavia Luz <flavialuz@MacBook-Pro-de-Flavia.local>
2026-05-18 15:01:10 -0300

    Implementa métodos de venda no ProdutosDAO

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -36,7 +36,57 @@ public class ProdutosDAO {
     }
     
     
-    
-        
+    public void venderProduto(int id) {
+
+    String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
+
+    conn = new conectaDAO().connectDB();
+
+    try {
+
+        prep = conn.prepareStatement(sql);
+
+        prep.setInt(1, id);
+
+        prep.execute();
+
+        JOptionPane.showMessageDialog(null, "Produto vendido!");
+
+    } catch (Exception e) {
+
+        JOptionPane.showMessageDialog(null, "Erro ao vender produto");
+    }
 }
+        
+public ArrayList<ProdutosDTO> listarProdutosVendidos() {
+
+    String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+
+    conn = new conectaDAO().connectDB();
+
+    try {
+
+        prep = conn.prepareStatement(sql);
+
+        resultset = prep.executeQuery();
+
+        while(resultset.next()) {
+
+            ProdutosDTO produto = new ProdutosDTO();
+
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+
+            listagem.add(produto);
+        }
+
+    } catch (Exception e) {
+
+        JOptionPane.showMessageDialog(null, "Erro ao listar produtos vendidos");
+    }
+
+    return listagem;
+}}
 
